5a20d46c04c5be3f6595e8fc532e08ab475849fe
Ella Julie Range <thecabbage999@gmail.com>
2025-01-31 15:44:28 -0600

    newcommit

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+
+
+  }
+  public void arcade_drive (){
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
